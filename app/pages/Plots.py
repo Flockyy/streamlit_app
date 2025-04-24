6b1d28546cb9b0d0b5e175fb6914043b3cd532c6
Florian <florian.abgrall750@gmail.com>
2025-04-24 13:55:10 +0200

    fix: data location

--- app/pages/Plots.py
+++ app/pages/Plots.py
@@ -15,3 +15,5 @@ st.title("Plots")
 st.bar_chart(df, x='affiliate', y='mean_salary', use_container_width=True)
 st.bar_chart(df, x='affiliate', y='lowest_salary', use_container_width=True)
 st.bar_chart(df, x='affiliate', y='highest_salary', use_container_width=True)
+
+st.altair_chart(df, x='affiliate', y='mean_salary', use_container_width=True)
